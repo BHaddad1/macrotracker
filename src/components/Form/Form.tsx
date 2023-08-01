55d168577a1b60aa435b89dbcc671e2efd77f5ad
Robert Aiken <robertsiraaiken@gmail.com>
2023-08-01 16:16:32 -0500

    Feat: Add get request, api key, and hide api key and basic home and form page

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,20 @@
+import {useState} from "react"
+import { getFood } from "../../apiCalls"
+
 const Form = () => {
+
+  const[food, setFood] = useState("")
+
+
   return (
     <form>
       <div>FORMMMMM</div>
+      <input className="food-input" type="text" name="food" onChange={e => setFood(e.target.value)} placeholder="Type food here"></input>
+      <button onClick={(e)=>{
+        e.preventDefault()
+        getFood(food)
+        .then(data=>console.log(data))
+      }}></button>
     </form>
   )
 }
