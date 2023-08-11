6583fa14b7a61d87de1f02dfc5c3a75cab4f1ad8
Blanche Haddad <blancherachel@gmail.com>
2023-08-11 14:45:58 -0600

    feat: Reformat

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -2,14 +2,14 @@ const API_KEY = import.meta.env.VITE_SOME_KEY
 
 export const getFood = async (query:string) => {
   const url = `https://api.api-ninjas.com/v1/nutrition?query=${query}`
-  const headers = {method: 'GET',
+  const headers = {
+    method: 'GET',
     headers: { 'X-Api-Key': `${API_KEY}`},
     contentType: 'application/json',
   }
-  const res = await fetch(url,headers);
+  const res = await fetch(url, headers);
   if (!res.ok){
     throw new Error("Unable to fetch macros")
-    console.log(res)
   }
   return res.json();
 }
