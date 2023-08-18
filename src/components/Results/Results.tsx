a9da231577fc605c05f4e3e80588c9b1892cdd36
Blanche Haddad <blancherachel@gmail.com>
2023-08-18 15:33:39 -0600

    feat: Import chart js, add pie chart for food info/data, install chart and react chart

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,13 +1,55 @@
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { 
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip, 
+  Legend
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement,
+  Tooltip, 
+  Legend);
 
 const Results = () => {
 
-
+  const location = useLocation(); 
+  const [foodInfo, setFoodInfo] = useState(location.state.foodInfo[0]);
+  
+  const data = {
+    labels: ['Fat', 'Protein', 'Carbs'],
+    datasets: [
+      {
+        label: "Macros for food in GRAMS",
+        data: [foodInfo.fat_total_g, foodInfo.protein_g, foodInfo.carbohydrates_total_g],
+        backgroundColor: [
+          'black',
+          'red',
+          'blue'
+        ],
+        borderWidth: 1,
+      }
+    ]
+  }
 
   return (
-
-    <div>RESULZTSSSS</div>
+    <>
+      <div style={{ textAlign: "center" }}>
+        <Pie
+          data={data}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Users Gained between 2016-2020"
+              }
+            }
+          }}
+  
+        />
+      </div>
+    </>
   )
 }
 
