a2bbf10f3b292dea0784cd6dc202c7469417f379
Blanche Haddad <blancherachel@gmail.com>
2023-11-21 16:06:18 -0700

    feat: Update styling for home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,10 +7,11 @@ const Home = () => {
   return (
     <main>
       <h1 className="header">MacroTracker</h1>
-      <NavLink to={"/form"} className="home-link">Get Started</NavLink>
-      <section className="about">About: </section>
-      <p className="home-about">Welcome to an easy way to track your macros!</p>
-      <p className="home-about">Simply fill out the form, view the results, and save your macros in a list.</p>
+      <section className="about">
+        <p className="home-about">Welcome to an easy way to track your macros!</p>
+        <p className="home-about">Simply fill out the form, view the results, and save your macros in a list.</p>
+      </section>
+      <NavLink to={"/form"} className="home-link">Get Started Here</NavLink>
     </main>
   )
 
